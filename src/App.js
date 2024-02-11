64e3d64bdcc760422775faff9bd851fd12c61e88
Arshad <arshadrauff008@gmail.com>
2024-02-11 23:27:10 +0530

    react router setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { BrowserRouter, Route, Routes, Na, Navigate } from "react-router-dom";
+
+// Pages
+import Login from "./pages/login/Login";
 
 const App = () => {
-  return <div className="App">Hello World {process.env.REACT_APP_API_KEY}</div>;
+  return <div className="App">
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Navigate to="login" />} />
+        <Route path="login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  </div>;
 };
 
 export default App;
